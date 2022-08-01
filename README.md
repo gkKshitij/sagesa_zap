6e887225a7202346c72b12a8232e650e52e33d77
Kshitij K <gk.kshitij@gmail.com>
2022-08-01 16:30:23 +0530

    updated README

--- README.md
+++ README.md
@@ -1,14 +1,80 @@
 # Zapier Platform for sagevfs
 
-TODO: **\*** VERY IMPORTANT **\*** : ask if the contents of lines [array] variable is important and if it should be included in the returning result (cause half of the variables are already there) as the functions will be needed to be modified accordingly.
+#### Some important good to know points that were discovered during Development of this project
 
-TODO: rename or define outputs in all functions if required
+- There are 4 main methods zapier-platform provides trigger(on new or update), create (a new or update), search & delete.
+- Looping is in beta stage
+- Folder structure that zapier recommends
+- many more that I forgot will add as I remember...
 
-TODO: in testing, test the search invoice method with invoice ID,
-because the response with and without ID parameter are different.
+###### FileStructure
 
-TODO: and all other usecases i.e. : Customer, Supplier, Receipts, Payments, Contacts
+<!-- prettier-ignore -->
+<pre>
+root
+|
+|--triggers
+|    |--new
+|    |    |--Invoice
+|    |    |--Customer
+|    |    |--Supplier
+|    |    |--Receipts
+|    |    |--Contacts
+|    |
+|    |--update
+|         |--Invoice
+|         |--Customer
+|         |--Supplier
+|         |--Receipts
+|         |--Contacts
+|
+|--create
+|    |--new
+|    |--update
+|
+|--search
+|    |--single
+|    |--multiple (through loops)
+|
+|--delete
+|    |--single
+|    |--multiple (through loops but might be complex)
+</pre>
 
-TODO: update for invoices
+## Completed Quests
 
-TODO: create invoices - WIP
+> DISCLAIMER : I don't know Javascript.
+
+**STORY LINE** : 20% completed (searches, triggers _{testing left as I ran out of zapier subscription}_ )
+_**This doesnt include DLC content.**_
+
+**MAIN QUESTS** :
+
+- [x] figured out how to loop to return multiple queries.
+- [x] learnt to write zapier tests and how they work
+
+## TODO:
+
+**STORY LINE**
+
+- [ ] and all other usecases i.e. : Customer, Supplier, Receipts, Payments, Contacts
+
+**MAIN QUESTS**
+
+- [ ] \*VERY IMPORTANT\* : ask if the contents of lines [array] variable is important and if it should be included in the returning result (cause half of the variables are already there) as the functions will be needed to be modified accordingly.
+
+**SIDE QUEST**
+
+- [ ] rename or define outputs in all functions if required [phase 1.5 😛]
+
+- [ ] While testing, test the search invoice method with invoice ID, because the response with and without ID parameter are different. (might turn out helpful)
+
+- [ ] Add link to postman collection
+
+- [ ] try and use filters to return the latest invoice in get/search invoices (optional but will be efficient and maybe even solution to some problems)
+
+> `http://[API URL]/api/[ver]/customer/Get?$skip=2&$top=20&$orderby=ID&apikey=39478ac6-ac2a-44d8-a31c-7e7e14af4de3&companyid=1`} quoted from [DeveloperProgram API](https://accounting.sageone.co.za/Marketing/DeveloperProgram.aspx)
+
+**DLC**
+
+- [ ] create & update for all will be second phase so not working right now.
--- creates/invoices.js
+++ creates/invoices.js
@@ -1,27 +1,25 @@
 // We recommend writing your creates separate like this and rolling them
 // into the App definition at the end.
 module.exports = {
-  key: "recipe",
+  key: "invoice",
 
   // You'll want to provide some helpful display labels and descriptions
   // for users. Zapier will put them into the UX.
-  noun: "Recipe",
+  noun: "Invoice",
   display: {
-    label: "Create Recipe",
-    description: "Creates a new recipe.",
+    label: "Create Invoice",
+    description: "Creates a new invoice.",
   },
 
   // `operation` is where the business logic goes.
   operation: {
     inputFields: [
-      { key: "name", required: true, type: "string" },
       {
         key: "directions",
         required: true,
         type: "text",
-        helpText: "Explain how should one make the recipe, step by step.",
+        helpText: "Explain how should one make the invoice, step by step.",
       },
-      { key: "authorId", required: true, type: "integer", label: "Author ID" },
       {
         key: "style",
         required: false,
@@ -31,7 +29,20 @@ module.exports = {
     ],
     perform: (z, bundle) => {
       const promise = z.request({
-        url: "https://auth-json-server.zapier-staging.com/recipes",
+        url: "https://accounting.sageone.co.za/api/2.0.0/TaxInvoice/Save?useSystemDocumentNumber={useSystemDocumentNumber}",
+
+        // Put the search value in a query param. The details of how to build
+        // a search URL will depend on how your API works.
+        options: {
+          params: {
+            companyid: bundle.inputData.companyid,
+            apiKey: bundle.inputData.apiKey,
+            id: bundle.inputData.id,
+
+            // $filter: "Counter eq '".concat(bundle.inputData.counter, "'"),
+          },
+        },
+
         method: "POST",
         body: {
           name: bundle.inputData.name,
@@ -39,6 +50,7 @@ module.exports = {
           authorId: bundle.inputData.authorId,
           style: bundle.inputData.style,
         },
+
         headers: {
           "content-type": "application/json",
 
@@ -56,12 +68,68 @@ module.exports = {
     // from the API, Zapier will fallback to this hard-coded sample. It should reflect the data structure of
     // returned records, and have obviously dummy values that we can show to any user.
     sample: {
-      id: 1,
-      createdAt: 1472069465,
-      name: "Best Spagetti Ever",
-      authorId: 1,
-      directions: "1. Boil Noodles\n2.Serve with sauce",
-      style: "italian",
+      DueDate: "2022-07-31T00:00:00Z",
+      FromDocument: "SO0000012",
+      FromDocumentId: 1497706259,
+      FromDocumentTypeId: 13,
+      Status: "Unpaid",
+      AllowOnlinePayment: true,
+      Paid: false,
+      Locked: false,
+      CustomerId: 39118490,
+      CustomerName: "cust 01",
+      Modified: "2022-07-07T12:00:56.18",
+      Created: "2022-07-07T12:00:56.18",
+      ID: 1497709240,
+      Date: "2022-07-07T00:00:00Z",
+      Inclusive: false,
+      DiscountPercentage: 0.0,
+      TaxReference: "",
+      DocumentNumber: "INV0000009",
+      Reference: "",
+      Message: "",
+      Discount: 0.0,
+      Exclusive: 12.0,
+      Tax: 1.8,
+      Rounding: 0.0,
+      Total: 13.8,
+      AmountDue: 13.8,
+      PostalAddress01: "",
+      PostalAddress02: "",
+      PostalAddress03: "",
+      PostalAddress04: "",
+      PostalAddress05: "",
+      DeliveryAddress01: "",
+      DeliveryAddress02: "",
+      DeliveryAddress03: "",
+      DeliveryAddress04: "",
+      DeliveryAddress05: "",
+      Printed: false,
+      Editable: true,
+      HasAttachments: false,
+      HasNotes: false,
+      HasAnticipatedDate: false,
+      Lines: [
+        {
+          SelectionId: 58993542,
+          TaxTypeId: 5984275,
+          ID: 1032023820,
+          Description: "apple",
+          LineType: 0,
+          Quantity: 1.0,
+          UnitPriceExclusive: 12.0,
+          Unit: "",
+          UnitPriceInclusive: 13.8,
+          TaxPercentage: 0.15,
+          DiscountPercentage: 0.0,
+          Exclusive: 12.0,
+          Discount: 0.0,
+          Tax: 1.8,
+          Total: 13.8,
+          Comments: "",
+          UnitCost: 10.0,
+        },
+      ],
     },
 
     // If the resource can have fields that are custom on a per-user basis, define a function to fetch the custom
